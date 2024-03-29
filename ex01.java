525283c0b0e763e5aee48ac6ca5110f2fcdcd197
pjeongwon <wjddnjs.dev@gmail.com>
2024-03-30 04:36:59 +0900

    test04

--- ex01.java
+++ ex01.java
@@ -2,6 +2,11 @@ public class ex01 {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
     System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
 
   }
 }
